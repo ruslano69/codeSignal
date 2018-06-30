6c61b0cbc1bcf837c4df294ca11f4a2da1e978ab
Mohammad Kebbi <kebbi@ualberta.ca>
2018-06-29 20:43:22 -0600

    weak numbers

--- Arcade/theCore/weakNumbers.py
+++ Arcade/theCore/weakNumbers.py
@@ -1,19 +1,23 @@
-def divisorCount(x):
-   divs = 1
-   i = 1
-   while i <= x:
-      if x % i == 0:
-         divs += 1
-      i += 1
-   return divs
-
 def weakNumbers(n):
-   nDiv = divisorCount(n)
-   weakest = divisorCount(1)
-   num_weakest = 0
-   for i in range(n):
-      iDiv = divisorCount(i)
-      if iDiv > nDiv and iDiv > weakest:
-         weakest = iDiv
-   
-   return weakest
+	rainbow = {1:1}
+	for i in range(2, n+1):
+		weaknesses = 1
+		for num in range(1, i):
+			if (float(i) / float(num)).is_integer():
+				rainbow[i] = weaknesses
+	weakest = 0
+	sameVal = 0
+	newRainbow = {}
+	for i in range(1, n + 1):
+		weakness = 0
+		for j in range(1, i):
+			if rainbow[i] < rainbow[j]:
+				weakness += 1
+		if weakness > weakest:
+			weakest = weakness
+		newRainbow[i] = weakness
+		
+	for k, v in newRainbow.items():
+		if weakest == v:
+			sameVal += 1
+	return [weakest, sameVal]
